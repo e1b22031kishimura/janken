0515052a892a4bad696071bd182e86bd38289712
e1b22031kishimura <e1b22031@oit.ac.jp>
2024-10-21 14:20:14 +0900

    DBの初期化処理を実装

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (userName) VALUES ('CPU');
+INSERT INTO users (userName) VALUES ('ほんだ');
+INSERT INTO matches (userName1,userName2,user1Hand,user2Hand) VALUES (2,1,'Gu','Choki');
+INSERT INTO matches (userName1,userName2,user1Hand,user2Hand) VALUES (2,1,'Gu','Gu');
+INSERT INTO matches (userName1,userName2,user1Hand,user2Hand) VALUES (2,1,'Gu','Pa');
