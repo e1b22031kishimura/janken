026fa56ac4e1d082d4972a2e30705b0bef297a48
e1b22031kishimura <e1b22031@oit.ac.jp>
2024-10-28 03:09:45 +0900

    ユーザ情報をSELECTで取得して表示

--- src/main/java/oit/is/z2310/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z2310/kaizi/janken/controller/JankenController.java
@@ -1,6 +1,7 @@
 package oit.is.z2310.kaizi.janken.controller;
 
 import java.security.Principal;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,42 +12,52 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import oit.is.z2310.kaizi.janken.model.Entry;
 
+import oit.is.z2310.kaizi.janken.model.User;
+import oit.is.z2310.kaizi.janken.model.UserMapper;
+
 @Controller
 @RequestMapping("/janken")
 public class JankenController {
 
   @Autowired
-  private Entry room;
+  UserMapper userMapper;
+
+  @GetMapping
+  public String redirectToStep1() {
+    return "redirect:/janken/step1";
+  }
 
   @GetMapping("step1")
-  public String Janken(ModelMap model, Principal prin) {
-    String loginUser = prin.getName();
-    this.room.addUser(loginUser);
-    model.addAttribute("room", this.room);
-    return "janken.html";
-  }
-
-  @GetMapping("battle")
-  public String battle(@RequestParam("hand") String userHand, ModelMap model) {
-    String cpuHand = "グー";
-    String result;
-
-    if (userHand.equals(cpuHand)) {
-      result = "引き分け";
-    } else if ((userHand.equals("グー") && cpuHand.equals("チョキ")) ||
-        (userHand.equals("チョキ") && cpuHand.equals("パー")) ||
-        (userHand.equals("パー") && cpuHand.equals("グー"))) {
-      result = "勝ち";
-    } else {
-      result = "負け";
-    }
-
-    model.addAttribute("room", this.room);
-    model.addAttribute("userHand", userHand);
-    model.addAttribute("cpuHand", cpuHand);
-    model.addAttribute("result", result);
+  public String Janken(ModelMap model) {
+    ArrayList<String> userNames = userMapper.selectAllUserName();
+    model.addAttribute("userNames", userNames);
 
     return "janken.html";
   }
+
+  /*
+   * @GetMapping("battle")
+   * public String battle(@RequestParam("hand") String userHand, ModelMap model) {
+   * String cpuHand = "グー";
+   * String result;
+   *
+   * if (userHand.equals(cpuHand)) {
+   * result = "引き分け";
+   * } else if ((userHand.equals("グー") && cpuHand.equals("チョキ")) ||
+   * (userHand.equals("チョキ") && cpuHand.equals("パー")) ||
+   * (userHand.equals("パー") && cpuHand.equals("グー"))) {
+   * result = "勝ち";
+   * } else {
+   * result = "負け";
+   * }
+   *
+   * model.addAttribute("room", this.room);
+   * model.addAttribute("userHand", userHand);
+   * model.addAttribute("cpuHand", cpuHand);
+   * model.addAttribute("result", result);
+   *
+   * return "janken.html";
+   * }
+   */
 
 }
